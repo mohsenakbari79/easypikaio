923827ee3e259b5742973832148b7d4f6b04633e
mohsen akbari <malevin.git@gmail.com>
2022-07-21 17:57:48 +0430

    update pipy package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Library for easy connection to rabbitmq  with pika Library'
 
 # Setting up
